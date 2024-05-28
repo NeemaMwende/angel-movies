c414a095d7866e9b4aecb54ac73007555a3223e8
NeemaMwende <gracepete12@gmail.com>
2024-05-28 19:54:40 +0300

    updates

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Form.css';
+import { useState } from 'react';
 
 const Form = () => {
+    const [notification, setNotification] = useState('');
+
+  const handleFocus = () => {
+    setNotification('Please fill this field');
+  };
+
+  const handleBlur = () => {
+    setNotification('');
+  };
+
   return (
     <div className='form-content'>
         <h1>Movie Search API</h1>
         <form>
             <input type="text" placeholder="Search for a movie" className='search-input'/>
-            <button className='search'>Search</button>
+            <button className='search'
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            >Search</button>
+            {notification && <p className="warning">{notification}</p>}
+
         </form>
     </div>
   )
