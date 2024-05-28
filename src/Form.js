38b7832f0519a40c96b82aabb46050b17f713b74
NeemaMwende <gracepete12@gmail.com>
2024-05-28 20:10:50 +0300

    updates

--- src/Form.js
+++ src/Form.js
@@ -13,6 +13,21 @@ const Form = () => {
     setNotification('');
   };
 
+  const form = document.querySelector('form');
+  const container = document.querySelector('.image-container');
+
+  form.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    let query = form.querySelector('input').value;
+    console.log(query);
+  });
+
+  async function tvMazeApi(query){
+    const req = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+    const res = await req.json();
+    console.log(res);
+}
+
   return (
     <div className='form-content'>
         <h1>Movie Search API</h1>
@@ -24,6 +39,9 @@ const Form = () => {
             >Search</button>
             {notification && <p className="warning">{notification}</p>}
 
+            <div className='image-container'>
+                
+            </div>
         </form>
     </div>
   )
